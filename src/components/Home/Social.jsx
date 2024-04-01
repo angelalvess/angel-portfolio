3a832fe79c0771957aff03175b97e67a11263f0b
angelalvess <angeltrashangel@gmail.com>
2024-04-01 12:52:36 -0300

    update project

---
+++ src/components/Home/Social.jsx
@@ -0,0 +1,29 @@
+const Social = () => {
+  return (
+    <div className="home-social">
+      <a
+        href="https://www.instagram.com/angell1000000/"
+        className="home-social-icon"
+        target={'_blank'}
+      >
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/angelalvess/"
+        className="home-social-icon"
+        target={'_blank'}
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://github.com/angelalvess"
+        className="home-social-icon"
+        target={'_blank'}
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
