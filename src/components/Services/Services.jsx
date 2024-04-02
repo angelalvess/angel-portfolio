f162d6e216c7d73924c0e7da1b287a7ad93f7be6
angelalvess <angeltrashangel@gmail.com>
2024-04-02 08:53:47 -0300

    update

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,202 @@
+import { useState } from 'react';
+import './Services.css';
+
+const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+  return (
+    <section className="services section" id="services">
+      <h2 className="section-title">Services</h2>
+      <span className="section-subtitle">What i offer</span>
+      <div className="services-container container grid">
+        {/* Services content 1*/}
+        <div className="services-content">
+          <div>
+            <i className="uil uil-web-grid services-icon"></i>
+            <h3 className="services-title">
+              Frontend <br />
+              Development
+            </h3>
+          </div>
+
+          <span className="services-button" onClick={() => toggleTab(1)}>
+            View More
+            <i className="uil uil-arrow-right services-button-icon"></i>
+          </span>
+
+          <div
+            className={
+              toggleState === 1
+                ? 'services-modal active-modal'
+                : 'services-modal'
+            }
+          >
+            <div className="services-modal-content">
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services-modal-close"
+              ></i>
+
+              <h3 className="services-modal-title">Frontend Development</h3>
+              <p className="services-modal-description">
+                Looking for the first job in the IT industry.Providing quality
+                work to clients and learning new technologies.
+              </p>
+
+              <ul className="services-modal-services grid">
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Web page development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Backend development.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Fullstack development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Services content 2 */}
+
+        <div className="services-content">
+          <div>
+            <i className="uil uil-arrow services-icon"></i>
+            <h3 className="services-title">
+              Backend <br /> Development
+            </h3>
+          </div>
+
+          <span className="services-button">
+            View More
+            <i className="uil uil-arrow-right services-button-icon"></i>
+          </span>
+
+          <div className="services-modal">
+            <div className="services-modal-content">
+              <i className="uil uil-times services-modal-close"></i>
+
+              <h3 className="services-modal-title">Backend Development</h3>
+              <p className="services-modal-description">
+                Looking for the first job in the IT industry.Providing quality
+                work to clients and learning new technologies.
+              </p>
+
+              <ul className="services-modal-services grid">
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Web page development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Backend development.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Fullstack development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        {/* Services content 3 */}
+
+        <div className="services-content">
+          <div>
+            <i className="uil uil-edit services-icon"></i>
+            <h3 className="services-title">
+              Fullstack <br /> Development
+            </h3>
+          </div>
+
+          <span className="services-button">
+            View More
+            <i className="uil uil-arrow-right services-button-icon"></i>
+          </span>
+
+          <div className="services-modal">
+            <div className="services-modal-content">
+              <i className="uil uil-times services-modal-close"></i>
+
+              <h3 className="services-modal-title">Fullstack Development</h3>
+              <p className="services-modal-description">
+                Looking for the first job in the IT industry.Providing quality
+                work to clients and learning new technologies.
+              </p>
+
+              <ul className="services-modal-services grid">
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Web page development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Backend development.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Fullstack development.</p>
+                </li>
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+
+                {/* ******** */}
+                <li className="services-modal-service">
+                  <i className="uil uil-check-circle services-modal-icon"></i>
+                  <p className="services-modal-info">Lorem.</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
