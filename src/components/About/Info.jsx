71c74903a8203d5728e517a962e247ad2a8493b7
angelalvess <angeltrashangel@gmail.com>
2024-04-04 10:00:44 -0300

    update project

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -10,7 +10,7 @@ function Info() {
       <div className="about-box">
         <i className="bx bx-briefcase-alt about-icon"></i>
         <h3 className="about-title">Completed</h3>
-        <span className="about-subtitle">48 + Projects</span>
+        <span className="about-subtitle">10 + Projects</span>
       </div>
 
       <div className="about-box">
